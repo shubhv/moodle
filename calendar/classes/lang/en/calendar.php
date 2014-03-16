c8e80a7b73d442d64c9baab940cffc727633f3da
Shubhangee Verma <shubhv@iitk.ac.in>
2014-03-16 16:55:11 +0530

    Definition of language string for the events names

---
+++ calendar/classes/lang/en/calendar.php
@@ -0,0 +1,6 @@
+<?php
+
+$string['eventsubscriptionadded'] = 'Calendar subscription added';
+$string['eventsubscriptionupdated'] = 'Calendar subscription updated';
+$string['eventsubscriptiondeleted'] = 'Calendar subscription deleted';
+
