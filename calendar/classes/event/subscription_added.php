3ce4c4bd1aa3db011028eb0c0ae979c703f202f4
Shubhangee Verma <shubhv@iitk.ac.in>
2014-03-16 16:51:41 +0530

    Event for Calendar Subscription added

---
+++ calendar/classes/event/subscription_added.php
@@ -0,0 +1,98 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Calendar subscription added event.
+ *
+ * @package    core
+ * @copyright  2014 Shubhangee Verma <shubhv92@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace core\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Calendar subscription added class.
+ *
+ * @package    core
+ * @copyright  2014 Shubhangee Verma <shubhv92@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class calendar_subscription_added extends base {
+
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        $this->data['crud'] = 'c'; // c(reate), r(ead), u(pdate), d(elete)
+        $this->data['edulevel'] = self::LEVEL_OTHER;
+        $this->data['objecttable'] = 'event_subscriptions';
+    }
+
+    /**
+     * Returns localised general event name.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return get_string('eventsubscriptionadded', 'calendar');
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return "User {$this->userid} has added a calendar subscription with id {$this->objectid}.";
+    }
+
+    /**
+     * Returns relevant URL.
+     *
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new \moodle_url('calendar/managesubscriptions.php', array('id' => $this->objectid));
+    }
+
+    /**
+     * Return legacy event name.
+     *
+     * @return string legacy event name.
+     */
+    public static function get_legacy_eventname() {
+        // Override ONLY if you are migrating events_trigger() call.
+        return 'subscription_added';
+    }
+
+    /**
+     * Return legacy event data.
+     *
+     * @return stdClass
+     */
+    protected function get_legacy_eventdata() {
+        // Override if you migrating events_trigger() call.
+        $data = new \stdClass();
+        $data->id = $this->objectid;
+        $data->userid = $this->relateduserid;
+        return $data;
+    }    
+}
